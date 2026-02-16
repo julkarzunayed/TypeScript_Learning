a8f3cd1a0abea31bade50f79328483b5d1898161
julkarzunayed <zunayedjulkar@gmail.com>
2026-02-16 07:36:42 +0600

    learned annotations and type inference

---
+++ First_TS_Project/src/typesInTs.ts
@@ -0,0 +1,16 @@
+// Type annotations and type inference
+
+let drink = "Tee";
+drink = "Coffee";
+// drink = 34; 
+
+let cups = Math.random() > 0.5 ? 1 : 2;
+// cups = "Two";
+
+let price = Math.random() > 0.5 ? 10 : "free";
+price = "Two";
+
+
+let order: boolean = true;
+order = false;
+// order = "Yes";
---
+++ project_setup.ts
@@ -0,0 +1,11 @@
+/**
+ * npm init -y ----[to initialize a new Node.js project with default settings]
+ * 
+ * npm install typescript --save-dev   / npm i -D typescript ----[to install TypeScript as a development dependency]
+ * 
+ * npx tsc --init -----[to create a tsconfig.json file with default settings]
+ * 
+ * npx tsc ----[to compile TypeScript files to JavaScript]
+ * 
+ * node dist/index.js ----[to run the compiled JavaScript file]
+ */
